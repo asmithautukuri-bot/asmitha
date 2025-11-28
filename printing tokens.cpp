d303adf1bfd7a1de27bb54df0b05d0e461130566
Asmitha <asmithautukuri@gmail.com>
2025-11-28 14:12:39 +0530

    Add files via upload

---
+++ printing tokens.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	char str[1001];
+	printf("enter a sentence:");
+	scanf("%[^\n]s",str);
+	printf("words in the sentence are:\n");
+	for (int i=0;str[i]!='\0'; i++)
+	{
+		if(str[i]==' ')printf("\n");
+		else printf("%c",str[i]);
+	}
+}
